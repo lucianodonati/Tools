c135cd2c15ad9e19436c396d2161d9f4b994d81b
Luciano Donati <me@lucianodonati.com>
2018-06-03 23:50:27 -0300

    Added ApplicationCommands.cs to call UnityEvents to load scenes, quit, etc.

---
+++ ApplicationCommands.cs
@@ -0,0 +1,33 @@
+﻿///—————————————————————–
+///   File: ApplicationCommands.cs
+///   Author: Luciano Donati
+///   me@lucianodonati.com	www.lucianodonati.com
+///   Last edit: 03-Jun-18
+///   Description: Class in charge of providing public methods for common calls on a Unity application. Usefull to load scenes from buttons or callbacks to exit the game, etc.
+///—————————————————————–
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Class in charge of providing public methods for common calls on a Unity application. Usefull to load scenes from buttons or callbacks to exit the game, etc.
+/// </summary>
+public class ApplicationCommands : MonoBehaviour
+{
+    /// <summary>
+    /// Loads a scene by Build Index
+    /// </summary>
+    /// <param name="sceneBuildIndex">The sceneBuildIndex<see cref="int"/></param>
+    public void LoadScene(int sceneBuildIndex)
+    {
+        SceneManager.LoadScene(sceneBuildIndex);
+    }
+
+    /// <summary>
+    /// Call to Application.Quit()
+    /// </summary>
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
