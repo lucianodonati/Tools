664a8bc8fab720aa3f3e93d119498f30e125eefa
Luciano Donati <me@lucianodonati.com>
2018-06-13 00:04:11 -0300

    Commented

--- ApplicationCommands.cs
+++ ApplicationCommands.cs
@@ -16,12 +16,23 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class ApplicationCommands : MonoBehaviour
 {
+    /// <summary>
+    /// Defines the <see cref="ProgressEvent"/> class for events with 1 float parameter.
+    /// </summary>
     [Serializable]
-    public class ProgressEvent : UnityEvent<float> { }
+    public class ProgressEvent : UnityEvent<float>
+    {
+    }
 
+    /// <summary>
+    /// Defines the progressEvent to subscribe to
+    /// </summary>
     [SerializeField]
     private ProgressEvent progressEvent = null;
 
+    /// <summary>
+    /// Defines the operation of async-loading (for progress)
+    /// </summary>
     private AsyncOperation async = null;
 
     /// <summary>
@@ -33,12 +44,20 @@ public class ApplicationCommands : MonoBehaviour
         SceneManager.LoadScene(sceneBuildIndex);
     }
 
+    /// <summary>
+    /// Loads a scene by Build Index asynchronously
+    /// </summary>
+    /// <param name="sceneBuildIndex">The sceneBuildIndex<see cref="int"/></param>
     public void LoadSceneAsync(int sceneBuildIndex)
     {
         progressEvent.Invoke(0);
         async = SceneManager.LoadSceneAsync(sceneBuildIndex);
     }
 
+    /// <summary>
+    /// Loads a scene by Build Index asynchronously and adds it
+    /// </summary>
+    /// <param name="sceneBuildIndex">The sceneBuildIndex<see cref="int"/></param>
     public void LoadSceneAsyncAdd(int sceneBuildIndex)
     {
         progressEvent.Invoke(0);
@@ -52,15 +71,19 @@ public class ApplicationCommands : MonoBehaviour
             progressEvent.Invoke(async.progress);
         }
     }
+
     /// <summary>
     /// Call to Application.Quit()
     /// </summary>
     public void QuitApplication()
     {
         Application.Quit();
-
     }
 
+    /// <summary>
+    /// The OpenURL
+    /// </summary>
+    /// <param name="URL">The URL<see cref="string"/></param>
     public void OpenURL(string URL)
     {
         Application.OpenURL(URL);
